2cf8f2077ec526fc5a916797e4d01b25f92aded3
Frederik Krogsdal Jacobsen <fkjac@dtu.dk>
2024-06-01 16:06:43 +0200

    Clean up code.

--- app/Main.hs
+++ app/Main.hs
@@ -1,16 +1,16 @@
 module Main where
 
-import qualified Data.HashTable.Class as C
-import qualified Data.HashTable.ST.Basic as H
-import Data.List (sortOn)
-import Data.Ord (Down(Down))
-import qualified Data.Text as T
+import Control.Exception (IOException, displayException, try)
 import Control.Monad.ST
-import Data.Either()
-import Control.Exception (try, IOException, displayException)
+import Data.Either ()
+import Data.HashTable.Class qualified as C
+import Data.HashTable.ST.Basic qualified as H
+import Data.List (sortOn)
+import Data.Ord (Down (Down))
+import Data.Text qualified as T
+import Data.Text.IO qualified as T.IO
 import System.Environment (getArgs)
 import System.Exit (exitFailure)
-import qualified Data.Text.IO as T.IO
 
 main :: IO ()
 main = do
@@ -25,20 +25,21 @@ main = do
           T.IO.putStr $ T.unlines $ map formatCount $ countAndSort $ T.words $ T.toCaseFold text
     _ -> T.IO.putStrLn (T.pack "Expected a file name as the only argument.") >> exitFailure
 
-formatCount :: (T.Text,Int) -> T.Text
-formatCount (word,count) = T.pack (show count <> ": ") <> word
+formatCount :: (T.Text, Int) -> T.Text
+formatCount (word, count) = T.pack (show count <> ": ") <> word
 
 countAndSort :: [T.Text] -> [(T.Text, Int)]
 countAndSort wordList = sortOn ordering $ countOccurrences wordList
--- We have to customize the comparator to sort in reverse order and descending on the counts
-  where ordering (word, count) = (Down count, word)
+  where
+    -- We have to customize the comparator to sort in reverse order and descending on the counts
+    ordering (word, count) = (Down count, word)
 
 countOccurrences :: [T.Text] -> [(T.Text, Int)]
 countOccurrences wordList = runST $ do
   table <- H.new
-  mapM_ (\ word -> C.mutate table word update) wordList
+  mapM_ (\word -> C.mutate table word update) wordList
   C.toList table
-  where update existing = case existing of
-                            Nothing -> (Just 1, ())
-                            Just oldCount -> (Just (oldCount + 1), ())
-
+  where
+    update existing = case existing of
+      Nothing -> (Just 1, ())
+      Just oldCount -> (Just (oldCount + 1), ())
